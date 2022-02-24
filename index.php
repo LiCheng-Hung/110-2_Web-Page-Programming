d11d3bfacc5f730c06d66546e2ed72f7067b0641
vivi61212102 <55627332+vivi61212102@users.noreply.github.com>
2022-02-24 16:56:03 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+    Week1 Homework
+   </head>
+  <body>
+  <?php
+    $i=10;
+    $j=20;
+    echo "Hello <B>PHP</B>!"."<br/>";
+    echo "i=10 j=20";
+    echo "<br/>";
+    echo "i+j=";
+    echo $i+$j;
+    echo "<br/>";
+    echo date("Y/m/d");
+    ?>
+  </body>
+</html>
