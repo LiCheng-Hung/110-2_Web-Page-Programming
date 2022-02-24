a41b6f112afe0828f41c671482487942a76f87c3
vivi61212102 <55627332+vivi61212102@users.noreply.github.com>
2022-02-24 17:07:44 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,14 @@
 <html>
   <head>
-    Week1 Homework
+    <h1>Week1 Homework</h1>
    </head>
   <body>
   <?php
     $i=10;
     $j=20;
-    echo "Hello <B>PHP</B>!"."<br/>";
-    echo "i=10 j=20";
-    echo "<br/>";
-    echo "i+j=";
-    echo $i+$j;
-    echo "<br/>";
+    echo "Hello <B>PHP</B>!</br>";
+    echo "i=10 j=20</br>";
+    echo "i+j=".$i+$j."<br/>";
     echo date("Y/m/d");
     ?>
   </body>
